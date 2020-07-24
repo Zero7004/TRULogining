71a4b073ab80c5c90368f8e15e05665a9afbb415
wangjianlong <wangjianlong@formssi.com>
2020-07-24 14:30:42 +0800

    Initial commit

---
+++ Example/TRULogining/WJLViewController.h
@@ -0,0 +1,13 @@
+//
+//  WJLViewController.h
+//  TRULogining
+//
+//  Created by wangjianlong on 07/24/2020.
+//  Copyright (c) 2020 wangjianlong. All rights reserved.
+//
+
+@import UIKit;
+
+@interface WJLViewController : UIViewController
+
+@end
